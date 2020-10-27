3106460e1f779f933a73eb0d3a6c8d2b4895cb35
StrappedGlint13 <matiasbrax@gmail.com>
2020-10-27 08:14:00 +0200

    Varasto 100% line coverage

--- ohtuvarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ ohtuvarasto/src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -64,5 +64,54 @@ public class VarastoTest {
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
+    
+    @Test
+    public void varastonArvotNegatiivinen() {
+        varasto.lisaaVarastoon(11);
+        assertEquals(0, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        
+        Varasto vara = new Varasto(-2, -2);
+        assertEquals(0, vara.paljonkoMahtuu(), vertailuTarkkuus);
+        assertEquals(0, vara.getSaldo(), vertailuTarkkuus); 
+        
+        Varasto vara2 = new Varasto(-1);
+        assertEquals(0, vara2.paljonkoMahtuu(), vertailuTarkkuus);
+       
+  
+    }
+    @Test
+    public void lisaaNegis() {
+        varasto.lisaaVarastoon(-1);
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastoAlustustusta() {
+        Varasto vara = new Varasto(10, 0);
+        assertEquals(10, vara.paljonkoMahtuu(), vertailuTarkkuus);
+        
+        Varasto vara2 = new Varasto(1, 2);
+        assertEquals(1, vara2.getSaldo(), vertailuTarkkuus);
+       
+    }
+    
+    
+    @Test
+    public void otaVarastostaToimii() {
+        varasto.otaVarastosta(-1);
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+        
+        varasto.lisaaVarastoon(3);
+        varasto.otaVarastosta(5);
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+        assertEquals(10, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void tulostusToimii() {
+        String merkkijonoesitys = "saldo = 0.0, vielä tilaa 10.0";
+        assertEquals(merkkijonoesitys, varasto.toString());
+    }
+
 
 }
